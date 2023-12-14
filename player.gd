df7b904c87c0b26965bf26a5880baa29ed163934
danicachow <dachow1@s.sfusd.edu>
2023-12-14 15:22:16 -0800

    world scene setup

--- player.gd
+++ player.gd
@@ -7,13 +7,13 @@ func _process(delta):
 	var velocity = Vector2.ZERO
 	
 	if Input.is_action_pressed("move_left"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_right"):
 		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
 	if Input.is_action_pressed("move_up"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_down"):
 		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
 	if velocity.length() > 0:
 		velocity = velocity.normalized()*speed
 		
